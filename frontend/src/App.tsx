0f57ef0243651d6184099de60243ed970e721467
nEunseo <nes020781@gmail.com>
2023-01-16 13:25:23 +0900

    feat: Main 페이지, App.jsx 라우터 세팅(재)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'; //라우터 세팅
+import Main from './Pages/Main';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          {/* 메인 페이지 */}
+          <Route path='/' element={<Main />} />
+
+          {/* 이하 다른 페이지들 주석으로 구분 */}
+          <Route />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/Pages/Main.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Main() {
+  return <div>Main page for test</div>;
+}
+
+export default Main;
