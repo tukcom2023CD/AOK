bec3b2c57179afe0e64c599ddeb0438b7f2cbf8d
Eunseo Na <nes020781@gmail.com>
2023-02-09 21:50:54 +0900

    feat:  컬러 팔레트 추가

---
+++ frontend/src/Components/MainPageBgr.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+/** MainPageBgr
+ * 페이지 퍼블리싱 상 거의 대부분의 페이지에 로고 + 프로필이 들어가기 때문에 공용 컴포넌트로 만드려 합니다
+ */
+
+export const MainBackground = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    
+`;
+
+export default function PageBackgr(){
+    return(
+        <div></div>
+    );
+}
---
+++ frontend/src/Components/MainProfile.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+
+export default function MainProfile(){
+    return(
+        <div></div>
+    );
+}
