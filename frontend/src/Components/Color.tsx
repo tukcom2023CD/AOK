cac1c0227b46864614e618112e0e61a32544277e
Eunseo Na <nes020781@gmail.com>
2023-02-09 21:52:49 +0900

    feat : 컬러 팔레트 수정

---
+++ frontend/src/Components/Color.tsx
@@ -0,0 +1,19 @@
+const theme = {
+    palette: {
+    Background: ['rgba(255,241,158,0.05)'],
+    
+    /**선택한 브랜치를 표시하는 노랑색 */
+    CrYellowSelect: ['rgba(255,241,158,0.6)'],
+    /**기본 노랑색 */
+    CrYellow: ['rgba(255,241,158,1)'],
+    /**기본 빨강색 */
+    CrRed: ['rgba(250,115,95,1)'],
+    
+    /**로그내역 확인시 속성에 맞는 컬러 */
+    LogGreen: ['rgba(157,224,160,1)'],
+    LogRed: ['rgba(237,175,156,1)'],
+    LogGray: ['rgba(217,217,217,1)'],
+    },
+};
+
+export default theme;
