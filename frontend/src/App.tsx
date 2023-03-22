0a1efead7022d5006f643702ddebd00cbfff1d11
sylveonpia <kyq0107@naver.com>
2023-03-23 00:29:44 +0900

    design/#37

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'; //라우터 세팅
 import Main from './Pages/Main';
-import Main1 from './Pages/Main1';
-import Log from './Pages/Log_/Log';
-import Login from './Pages/Member/Login';
-import Upload from './Pages/Upload';
-import Repository from './Pages/Repository/Repository';
-import Branch from './Pages/Branch/Branch';
-import RepositorySetting from './Pages/Repository/RepositorySetting';
-import Notice from './Pages/Notice/Notice';
-import UpsideBar from './Components/UpsideBar';
-
 function App() {
   return (
     <div className="App">
@@ -21,14 +11,6 @@ function App() {
 
           {/* 이하 다른 페이지들 주석으로 구분 */}
           <Route path='/main' element={<Main />} />
-          <Route path='/up' element = {<UpsideBar/>} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/upload' element={<Upload />} />
-          <Route path='/Log' element={<Log/>} />
-          <Route path='/repository' element={<Repository/>} />
-          <Route path='/repository_setting' element={<RepositorySetting/>} />
-          <Route path='/branch' element={<Branch/>} />
-          <Route path='/notice' element={<Notice/>} />
         </Routes>
       </Router>
     </div>
--- frontend/src/Main.tsx
+++ frontend/src/Main.tsx
@@ -2,9 +2,9 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import { ThemeProvider,createTheme } from '@mui/material/styles';
 import styled from 'styled-components';
-import UpsideBar from '../Components/UpsideBar';
+import UpsideBar from './Components/UpsideBar';
 import { Typography, Button } from '@mui/material';
-import ImgList from '../Components/ImgList';
+import ImgList from './Components/ImgList';
 
 export const theme = createTheme({
     palette: {
