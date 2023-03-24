8376ed9a8f4b18f413579dd9dcc9cfc7e82f471d
Eunseo Na <nes020781@gmail.com>
2023-03-24 22:01:37 +0900

    design: Button 컴포넌트 재사용성 보완

--- frontend/src/Components/Modalcom.tsx
+++ frontend/src/Components/Modalcom.tsx
@@ -4,6 +4,7 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import { borderRadius } from '@mui/system';
+import styled from 'styled-components';
 
 const style = {
   position: 'absolute' as 'absolute',
@@ -18,6 +19,25 @@ const style = {
   boxShadow: 24,
   p: 4,
 };
+
+const InputBox = styled.div`
+  text-align: center;
+  margin-top: 8rem;
+`;
+
+const InputStyle = styled.input`
+  width: 630px;
+  height: 75px;
+  border: 1px solid #CFCFCF;
+  border-radius: 10px;
+  font-size: 20pt;
+  font-weight: bold;
+  padding: 1.5rem;
+`;
+
+const BtnBox = styled.div`
+  text-align: center;
+`;
 
 export default function BasicModal() {
   const [open, setOpen] = React.useState(false);
@@ -34,11 +54,18 @@ export default function BasicModal() {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h3" component="h2" align="center">
+          <Typography id="modal-modal-title" sx={{fontWeight: 'bold'}} variant="h3" component="h2" align="center">
             Project
           </Typography>
-          
-          
+          <InputBox>
+            <Typography id="modal-modal-title" sx={{fontWeight: 'bold'}} variant="h6" component="h2" align="left" marginLeft={'125px'}>
+              name
+            </Typography>
+            <InputStyle/>
+          </InputBox>
+          <BtnBox>
+            
+          </BtnBox>
         </Box>
       </Modal>
     </div>
