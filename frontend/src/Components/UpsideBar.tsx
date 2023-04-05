b0d9c189697212997435324cea72c9133a9f6c36
Eunseo Na <nes020781@gmail.com>
2023-04-05 23:20:12 +0900

    refactor: 상단바 styled-component로 리팩토링

--- frontend/src/Components/UpsideBar.tsx
+++ frontend/src/Components/UpsideBar.tsx
@@ -1,63 +1,45 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import { ThemeProvider,createTheme } from '@mui/material/styles';
+import crepeimg from '../Images/crepeimg.png';
 import styled from 'styled-components';
-import Profile from '../Components/Profile';
 
+const BackgrDiv = styled.div`
+    display: flex;
+    width: 100%;
+    height: 65px;
+    background-color: #FF9198;
+`;
 
-export const theme = createTheme({
-    palette: {
-    primary: {
-        main: "#FF9198",
-    },
-    secondary: {
-        main: "#000000",
-    },
-    },
-});
+const LogoDiv = styled.div`
+    display: flex;
+    margin-left: 20px;
+    align-items: center; 
+`;
+
+const ImageDiv = styled.div`
+    width: 50px;
+    height: 50px;
+    background-image: url(${crepeimg});
+    background-position: center;
+    background-size: 50px;
+    background-repeat: no-repeat;
+`; 
+
+const TitleDiv = styled.div`
+    font-size: 20pt;
+    font-weight: bold;
+    color: white;
+    margin-left: 15px;
+`;
+
 
 export default function UpsideBar() {
-    const [auth, setAuth] = React.useState(true);
-    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setAuth(event.target.checked);
-    };
-
-    const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-    };
-
-    const handleClose = () => {
-    setAnchorEl(null);
-    };
-
-    return (
-        <ThemeProvider theme={theme}>
-        <Box sx={{ flexGrow: 1 } } >
-        
-        <AppBar position="static" color='primary'>
-        <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            {/* <img src="img/crepe.png" alt="crepe" /> */}
-            crepe
-            </Typography>
-            {auth && (
-            <Box>
-                
-            </Box>
-            )}
-        </Toolbar>
-        </AppBar>
-
-        <section>
-        </section>
-    </Box>
-    </ThemeProvider>
+    return(
+        <BackgrDiv>
+            <LogoDiv>
+                <ImageDiv/>
+                <TitleDiv>Crepe</TitleDiv>
+            </LogoDiv>
+            
+        </BackgrDiv>
     );
 }
