7c74ba4b20b7f4276a653e0722168d5f1bbf5085
Juyeon <kyq0107@naver.com>
2023-04-12 21:02:50 +0900

    design: 메인페이지 내용 위치 중앙 조절

--- frontend/src/Pages/Main.tsx
+++ frontend/src/Pages/Main.tsx
@@ -72,7 +72,7 @@ export default function Main() {
                         
                         <Box display={'flex'} justifyContent={'center'} alignItems={'center'}>
                         <Box display={'flex'} justifyContent={'center'}  width={'20%'}></Box>
-                            <Box display={'flex'} justifyContent={'center'}  width={'60%'}><Typography variant='h4' fontWeight={'bold'} alignContent={'center'} >Projects</Typography></Box> 
+                            <Box display={'flex'} justifyContent={'center'}  width={'60%'}><Typography variant='h4' fontWeight={'bold'} >Projects</Typography></Box> 
                             <Box width={'20%'} display={'flex'} justifyContent={'center'}>
                                 <CreateBtn>create</CreateBtn>
                             </Box>
