4a31eb8f516897634cbe4d651f5eb68d56ed789b
taeyang0206 <104223829+taeyang0206@users.noreply.github.com>
2023-04-17 21:32:24 +0900

    Update ResultCode.java

--- backend/src/main/java/crepe/backend/global/response/ResultCode.java
+++ backend/src/main/java/crepe/backend/global/response/ResultCode.java
@@ -8,11 +8,11 @@ import lombok.Getter;
 public enum ResultCode {
 
     // user
-    CREATE_USER_SUCCESS("U001", "사용자 추가 성공"),
-    READ_ONE_USER_SUCCESS("U003", "사용자 정보 조회 성공"),
-    UPDATE_USER_SUCCESS("U004", "사용자 정보 수정 성공"),
-    DELETE_USER_SUCCESS("U005", "사용자 삭제 성공"),
-    READ_ALL_USER_PROJECT_SUCCESS("U006", "전체 프로젝트 조회 성공"),
+    CREATE_USER_SUCCESS(200, "U001", "사용자 추가 성공"),
+    READ_ONE_USER_SUCCESS(200, "U003", "사용자 정보 조회 성공"),
+    UPDATE_USER_SUCCESS(200, "U004", "사용자 정보 수정 성공"),
+    DELETE_USER_SUCCESS(200, "U005", "사용자 삭제 성공"),
+    READ_ALL_USER_PROJECT_SUCCESS(200, "U006", "전체 프로젝트 조회 성공"),
     
     // project
     CREATE_PROJECT_SUCCESS(200,"P001", "프로젝트 생성 성공"),
