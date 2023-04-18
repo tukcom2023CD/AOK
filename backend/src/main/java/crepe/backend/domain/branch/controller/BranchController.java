5571dbcc659e67fb11e021c0ddbab6fb14304590
taeyang0206 <taengsun0206@gmail.com>
2023-04-18 20:30:27 +0900

    UserService 철자 오류 해결

--- backend/src/main/java/crepe/backend/domain/branch/controller/BranchController.java
+++ backend/src/main/java/crepe/backend/domain/branch/controller/BranchController.java
@@ -1,21 +1,3 @@
-/*
-package crepe.backend.domain.branch.controller;
-
-import crepe.backend.global.response.ResultResponse;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/v1/branches")
+/*package crepe.backend.domain.branch.controller;
 public class BranchController {
-
-
-}
-*/
+}*/
