bdc69880ce812dcab9bc3eea160f1816361946d6
Ellie <doris0707@naver.com>
2023-04-18 21:42:13 +0900

    feat: create log api result code setting

--- backend/src/main/java/crepe/backend/domain/log/controller/LogController.java
+++ backend/src/main/java/crepe/backend/domain/log/controller/LogController.java
@@ -1,7 +1,20 @@
 package crepe.backend.domain.log.controller;
 
-import org.springframework.stereotype.Controller;
+import crepe.backend.global.response.ResultResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RequestMapping("/api/v1/logs")
+@RestController
+@RequiredArgsConstructor
 public class LogController {
+
+    @PostMapping
+    public ResponseEntity<ResultResponse> createLog() {
+
+    }
+
 }
