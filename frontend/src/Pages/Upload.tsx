4c620a912bbf543a14cc8c724539e53be022eabe
Eunseo Na <nes020781@gmail.com>
2023-04-27 10:39:06 +0900

    feat: upload 페이지 안 쓰는 컴포넌트들 제거

--- frontend/src/Pages/Upload.tsx
+++ frontend/src/Pages/Upload.tsx
@@ -11,44 +11,6 @@ import UpsideGray from '../Components/UpsideGray';
 import DragDrop from '../Components/DragDrop/DragDrop';
 import ProjectSelect from '../Components/ProjectSelect';
 
-const Backgrdiv = styled.div`
-  display: inline-block;
-  width: 100%;
-  background-color: white;
-  text-align: center;
-`; 
-
-
-const Commentdiv = styled.input`
-  width: 1000px;
-  height: 85px;
-  border-radius: 10px;
-  background-color: #FFF4F4;
-  border: 3px solid #FFDEDE;
-  font-size: 20pt;
-  font-weight: bold;
-  padding: 1.5rem;
-  //box-shadow: 2px 4px 8px;
-`;
-
-const Btndiv = styled.div`
-  margin-top: 5rem;
-  margin-bottom: 10rem;
-`;
-
-const ApplyBtn = styled.button `
-  width: 250px;
-  height: 75px;
-  background-color: #FF9198;
-  color: white;
-  font-size: 18pt;
-  font-weight: bold;
-  border-radius: 20px;
-  &:hover{
-    background-color: #d7777e;
-    transition: 0.5s;
-  }
-`;
 
 export const theme = createTheme({
   palette: {
