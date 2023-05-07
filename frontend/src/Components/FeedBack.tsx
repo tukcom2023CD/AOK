9c5eb8f47fd67c1a913d2638d16412580c980871
Eunseo Na <nes020781@gmail.com>
2023-05-08 00:28:45 +0900

    chore: 불필요한 컴포넌트 및 Pages 폴더 밖에 있는 Main.tsx 파일 삭제

--- frontend/src/Components/FeedBack.tsx
+++ frontend/src/Components/FeedBack.tsx
@@ -1,21 +1,8 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import { ThemeProvider,createTheme } from '@mui/material/styles';
-import UpsideBar from '../Components/UpsideBar';
 import { Typography, Button, Paper } from '@mui/material';
-import BasicSelect from '../Components/ProjectSelect';
-import BasicList from '../Components/List';
-import AddIcon from '@mui/icons-material/Add';
-import SettingsIcon from '@mui/icons-material/Settings';
-import Divider from '@mui/material/Divider';
-import Profile from '../Components/Profile';
 import styled from 'styled-components';
-import SelectBar from '../Components/SelectBar';
-import EditIcon from '@mui/icons-material/Edit';
-import TextField2 from '../Components/TextField2';
-import DiscreteSlider from '../Components/Slider';
-import InteractiveList from '../Components/FileList';
-import { bgcolor, borderColor, fontSize } from '@mui/system';
+
 
 interface props{
     backgroundcolor?: string;
